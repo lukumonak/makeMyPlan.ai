b19b12424cacc7cb7ec427ccde41f38290673179
Luku <lukumonidoloi88451@gmail.com>
2025-03-07 18:22:31 +0530

    update3

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -7,17 +7,18 @@ import Footer from './Footer';
 function Hero() {
   return (
     <>
-    <div className='flex flex-col items-center mx-56 gap-9'> 
-      <h1 className='font-extrabold text-[50px] text-center mt-16 text-[#3141b9]' > <span className='text-[#FF7A00]'>Plan your perfect trip with our AI-powered travel website</span> smart recommendations, seamless bookings, and hassle-free adventures!</h1>
+    <div className='w-full h-screen bg-cover bg-center  ' style={{ backgroundImage: "url('/mtcty.jpg')" }}> 
+    <div className='flex flex-col items-center mx-56 gap-9 text-[#FFFFFF] ' > 
+      <h1 className='font-extrabold text-[50px] text-center mt-16 text-[#FFFFFF]' > <span className='text-[#FFA500]'>Plan your perfect trip with our AI-powered travel website</span> smart recommendations, seamless bookings, and hassle-free adventures!</h1>
       <p>Discover new destinations with AI-driven recommendations tailored just for you. Effortlessly plan, book, and explore with real-time travel insights. Experience stress-free journeys with our smart travel assistant!</p>
 
       <Link to={'/planner'}>
 
-      <Button className='cursor-pointer p-5'>Let's plane a trip</Button>
+      <Button className='cursor-pointer p-5 border-3' variant="ghost">Let's plane a trip</Button>
       </Link>
-
     </div>
-      <img src="/travel.png"  className='w-full -mt-100 -z-10 opacity-30' alt="tavel" />
+    </div>
+
     </>
    
   );
